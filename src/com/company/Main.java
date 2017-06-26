2eb9a7f4b54b9e924c29f8981fb6b036461b3218
Matthew Menna <mattmenna@gmail.com>
2017-06-26 11:31:39 -0400

    Changed public methods to private

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,67 @@
 package com.company;
 
+/*
+Calculate the perimeter and area of various classrooms at Grand Circus based on user input.
+
+Build Specs:
+Assume that the rooms are perfect retangles.
+Assume that user will enter alid numeric data for length and width.
+Accept decimal entries (12.5, for instance
+ */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Welcome to Grand Circus' Room Detail Generator!");
+        System.out.println();
+        float length;
+        float width;
+        float height;
+
+        float aOutput;
+        float pOutput;
+        float vOutput;
+
+        char cont;
+
+        do {
+            System.out.print("Enter Length: ");
+            length = scnr.nextFloat();
+            System.out.print("Enter Width: ");
+            width = scnr.nextFloat();
+            System.out.print("Enter Height: ");
+            height = scnr.nextFloat();
+
+            aOutput = area(length, width);
+            pOutput = perimeter(length, width);
+            vOutput = volume(length, width, height);
+
+            System.out.println("Area is " + aOutput);
+            System.out.println("Perimeter is " + pOutput);
+            System.out.println("Volume is " + vOutput + "\n");
+
+            System.out.println("Do you want to continue? Please use y or n");
+            cont = scnr.next().charAt(0);
+
+            } while(cont != 'n');
+        }
+
+    private static float area(float length, float width) {
+        float area = length * width;
+        return area;
+    }
+
+    private static float perimeter(float length, float width) {
+        float perimeter = 2 * length + 2 * width;
+        return perimeter;
+    }
+
+    private static float volume(float length, float width, float height) {
+        float volume=0;
+        return volume;
     }
 }
